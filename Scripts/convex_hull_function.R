7084ee0a66cf772e9b6db45db818d83719722a47
WebbA1 <aaronwebb15@gmail.com>
2019-07-28 20:50:38 +0100

    Commit - Aaron - RStudio

--- Scripts/convex_hull_function.R
+++ Scripts/convex_hull_function.R
@@ -5,24 +5,48 @@ crossprod <- function(o, a, b) {
 
 
 montone_chain_algo <- function(longitude_coord, latitude_coord) {
-  browser()
   coordMatrix <- cbind(longitude_coord, latitude_coord)
   # Find Min and Max X Coord - Longitude
-  points <- unique(latLongCoord[sort(latLongCoord[1:nrow(latLongCoord)]), 1:2])
+  points <- unique(coordMatrix[order(coordMatrix[1:nrow(coordMatrix)]), 1:2])
   # Build lower hull
   lower <- c()
   for(row in 1:nrow(points)) {
-    while(length(lower) >= 2 & crossprod(lower[nrow(lower), 1:2], 
-                                         lower[nrow(lower) - 1, 1:2], 
-                                         row) <= 0) {
-      print("Hello")
-      
+    while(length(lower)/2 >= 2) {
+      if(crossprod(lower[nrow(lower), 1:2],
+                lower[nrow(lower) - 1, 1:2],
+                points[row, 1:2]) <= 0) {
+        lower <- lower[-nrow(lower), 1:2]
+      } else {break}
     }
+    lower <- rbind(lower, points[row, 1:2])
   }
+  # Build Upper Hull
+  upper <- c()
+  points <- unique(coordMatrix[order(coordMatrix[1:nrow(coordMatrix)], decreasing = T), 1:2])
+  # Need to reverse the points now
+  for(row in 1:nrow(points)) {
+    while(length(upper)/2 >= 2) {
+      if(crossprod(upper[nrow(upper), 1:2],
+                   upper[nrow(upper) - 1, 1:2],
+                   points[row, 1:2]) <= 0) {
+        upper <- upper[-nrow(upper), 1:2]
+      } else {break}
+    }
+    upper <- rbind(upper, points[row, 1:2])
+  }
+  return(rbind(upper[-nrow(upper), 1:2], lower[-nrow(lower), 1:2]))
 }
 test <- montone_chain_algo(lon_coord, lat_coord)
+#test <- chull(lon_coord, lat_coord)
 p <- Polygon(test)
 ps <- Polygons(list(p), 1)
 sps <- SpatialPolygons(list(ps))
 plot(sps)
 
+leaflet(sps) %>% 
+  addTiles() %>%
+  addPolygons()
+
+ggplot() +
+  geom_point(mapping = aes(x = latLongCoord[, 1], y = latLongCoord[, 2])) +
+  geom_point(mapping = aes(x = test[, 1], y = test[, 2], color = "red"))
--- Scripts/initial_script.R
+++ Scripts/initial_script.R
@@ -7,11 +7,12 @@ rawData <- read_csv("Data/postcodes.csv")
 # Filter so we only have active postcodes in aberdeen city
 activePostcodes <- rawData %>%
   filter(`In Use?` == "Yes") %>% 
-  filter(District == "Aberdeen City")
+  filter(Country == "Scotland") %>% 
+  filter(Longitude != 0 & Latitude != 0) 
 # Take the lat and long coords for Aberdeen City
 lat_coord <- activePostcodes$Latitude 
 lon_coord <- activePostcodes$Longitude
-lon_coord <- sort(lon_coord)
+
 # Bind into a matrix
 latLongCoord <- cbind(lon_coord, lat_coord)
 p <- Polygon(latLongCoord)
